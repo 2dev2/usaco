83dc88e6a723b276f2b9e80b085f597c299120e5
2dev2 <2dev2@users.noreply.github.com>
2014-10-12 23:49:59 +0530

    Create usaco1_ride.cpp

---
+++ usaco1_ride.cpp
@@ -0,0 +1,54 @@
+/* ID: dharmad1
+PROG: ride
+LANG: C++
+*/
+
+
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <set>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <cassert>
+#include <string>
+#include <cstring>
+using namespace std;
+//const int mod = 1000000007;
+//const int N = 100001;
+#define rep(i,a,b) for(int i = a; i < b; i++) //rep(i,1,n+1)
+#define S(x) scanf("%d",&x)
+#define P(x) printf("%d\n",x)
+#define ff first
+#define ss second
+
+typedef long long ll;//int64
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef pair<int, int> pii;
+typedef set<int> si;
+typedef map<string, int> msi;
+
+int main()
+{
+
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+    string a,b;
+    fin>>a>>b;
+
+
+    ll resa=1;
+    ll resb=1;
+    rep(i,0,a.size())  {resa*=(a[i]-'A'+1); resa=resa%47;}
+    rep(i,0,b.size())  {resb*=(b[i]-'A'+1);resb=resb%47;}
+
+    if(resa==resb) fout<<"GO"<<endl;
+    else fout<<"STAY"<<endl;
+
+
+
+    return 0;
+}
